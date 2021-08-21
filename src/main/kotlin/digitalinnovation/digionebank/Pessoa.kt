e01cad2922cf597632105eb1af993a49ecc71f93
Luna <86981168+lunatila@users.noreply.github.com>
2021-08-21 14:26:18 -0300

    first commit

--- src/main/kotlin/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/digitalinnovation/digionebank/Pessoa.kt
@@ -1,4 +1,15 @@
 package digitalinnovation.digionebank
 
 class Pessoa {
+    var nome: String = "Atila"
+    var idade: Int = 25
+    var cpf: String = "123.456.789-10"
+}
+
+fun main(){
+    val atila = Pessoa()
+
+    println(atila.nome)
+    println(atila.idade)
+    println(atila.cpf)
 }
